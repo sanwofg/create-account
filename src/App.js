5c1c75fa8734c0980b7838e61a83db28e4d89923
Oladapo Olusola <Oladapo.O@bullseye.local>
2023-05-24 14:42:37 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Header from './Header';
+import Body from './Body';
+import './index.css'
+
+function App() {
+  return (
+    <div className='appp'>
+       <Header />
+       <Body />
+    </div>
+  );
+}
+
+export default App;
